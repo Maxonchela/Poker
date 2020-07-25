720b380668f66fb048d35f9f5d37f1c190e49d64
Mr Whale <maxonchela@gmail.com>
2020-07-25 18:55:22 +0300

    Card Recognition

---
+++ src/main/java/ChangeName.java
@@ -0,0 +1,15 @@
+import java.io.File;
+
+public class ChangeName {
+    public void change(){
+        File[] filesList;
+        File filesPath = new java.io.File("/Users/max/Desktop/for Poker/imgs"); // создаем объект на папку с файлами
+        filesList = filesPath.listFiles(); // записываем файлы из папки в массив объектов типа File
+
+        for(int i=0; i<filesList.length; i++){
+            String name = filesList[i].getName();
+            filesList[i].renameTo(new File("/Users/max/Desktop/for Poker/imgs" + i + ".png"));
+
+        }
+    }
+}
